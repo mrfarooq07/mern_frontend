9afca151f2e6592260b192dfaa0ed0c924129e2e
Mr-Farooq-599 <farooqarshad519@gmail.com>
2023-07-05 15:53:07 +0500

    message section started
    message section started

--- src/components/SingleChat.js
+++ src/components/SingleChat.js
@@ -1,13 +1,67 @@
 import React from "react";
 import { ChatState } from "../Context/ChatProvider";
-import { Box, IconButton, Text } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  IconButton,
+  Input,
+  Spinner,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import { getSender, getSenderFull } from "../config/ChatLogics";
 import ProfileModel from "./miscellaneous/ProfileModel";
 import UpdateGroupNameModal from "./miscellaneous/UpdateGroupNameModal";
+import { useState } from "react";
+import axios from "axios";
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoadinf] = useState(false);
+  const [newMessages, setNewMessages] = useState();
   const { user, selectedChat, setSelectedChat } = ChatState();
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  const toast = useToast();
+
+  const sendMessage = async (event) => {
+    if (event.key === "Enter" && newMessages) {
+      try {
+        const config = {
+          headers: {
+            "Content-type": "application/json",
+            Authorization: `Bearer ${userInfo.token}`,
+          },
+        };
+
+        const { data } = await axios.post(
+          "/api/message",
+          {
+            content: newMessages,
+            chatId: selectedChat._id,
+          },
+          config
+        );
+        console.log(data);
+        setNewMessages("");
+        setMessages([...messages, data]);
+      } catch (error) {
+        toast({
+          title: "Unable to send the message",
+          description: error.message,
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+          position: "top-right",
+        });
+      }
+    }
+  };
+  const typingHandler = async (e) => {
+    setNewMessages(e.target.value);
+    // typing indicator logic here
+  };
+
   return (
     <>
       {selectedChat ? (
@@ -56,7 +110,26 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
             borderRadius={"lg"}
             overflow={"hidden"}
           >
-            {/* messages here */}
+            {loading ? (
+              <Spinner
+                size={"xl"}
+                w={20}
+                h={20}
+                alignSelf={"center"}
+                margin={"auto"}
+              />
+            ) : (
+              <div>{}</div>
+            )}
+            <FormControl onKeyDown={sendMessage} isRequired mt={3}>
+              <Input
+                variant={"filled"}
+                bg={"#E0E0E0"}
+                placeholder="Write a message..."
+                onChange={typingHandler}
+                value={newMessages}
+              />
+            </FormControl>
           </Box>
         </>
       ) : (
